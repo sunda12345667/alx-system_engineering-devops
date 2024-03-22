e65cc70cd4fe7dd4c293c348a7b2e80d01bde8f2
sunda12345667 <sundayadenuga320@gmail.com>
2024-03-22 13:56:19 -0700

    pis

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# Create a file with puppet
+# creates a file in /tmp
 
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
