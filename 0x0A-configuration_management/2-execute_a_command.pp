e1d4a6d69da6567204901dd83675b9b246194ca2
sunda12345667 <sundayadenuga320@gmail.com>
2024-03-22 14:10:37 -0700

    jk

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,14 +1,6 @@
-#using puppet to install flask from pip3
+# kill process killmenow
 
-# It installs flask from pip3
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-}
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['werkzeug'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
